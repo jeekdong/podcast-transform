e72ed8d4296f24e104956e7e8b8e9ca1848d7614
jeekdong <jeekdong@gmail.com>
2025-03-10 01:53:10 +0800

    feat: add fetch header

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,7 +1,22 @@
 export async function getAudioUrl(url: string) {
   if (url.includes('www.xiaoyuzhoufm.com/episode')) {
     // 小宇宙 fm 的链接
-    const response = await fetch(url)
+    const headers = {
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
+      Accept:
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+      Referer: 'https://www.xiaoyuzhoufm.com/',
+      'Sec-Fetch-Dest': 'document',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Site': 'same-origin',
+      'Sec-Fetch-User': '?1',
+      'Upgrade-Insecure-Requests': '1',
+      'Cache-Control': 'no-cache',
+      Pragma: 'no-cache',
+    }
+    const response = await fetch(url, { headers })
     const html = await response.text()
     // 正则匹配出 audio 的 url
     const audioUrl = html.match(
